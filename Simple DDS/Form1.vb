2c42968eb30110637527a259076434a960828930
Walker <33885782+WalkerMx@users.noreply.github.com>
2026-03-31 09:09:28 -0500

    Add files via upload

---
+++ Simple DDS/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Using OFD As New OpenFileDialog With {.Filter = "Image Files|*.png;*.jpg;*.bmp"}
+            If OFD.ShowDialog = DialogResult.OK Then
+                Dim AlphaMode As Integer = 0
+                If NoAlphaRB.Checked = True Then AlphaMode = 0
+                If OneBitAlphaRB.Checked = True Then AlphaMode = 1
+                If EightBitAlphaRB.Checked = True Then AlphaMode = 2
+                Using DDSTestImage As New DDS(Image.FromFile(OFD.FileName), AlphaMode, CompressionCheckBox.Checked, MipMapCheckBox.Checked, ExtendedHeaderCheckBox.Checked, QualityCheckBox.Checked)
+                    Using SFD As New SaveFileDialog With {.Filter = "DDS Files|*.dds|All Files|*.*"}
+                        If SFD.ShowDialog = DialogResult.OK Then
+                            DDSTestImage.SaveImage(SFD.FileName)
+                        End If
+                    End Using
+                End Using
+            End If
+        End Using
+
+    End Sub
+
+End Class
